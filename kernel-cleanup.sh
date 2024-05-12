bf56324f4d265c15b387e84558429e701b43cb47
Alexia Steinberg <lachicadesistemas@gmail.com>
2024-05-12 11:24:53 -0300

    new version

--- kernel-cleanup.sh
+++ kernel-cleanup.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 # Kernel Cleanup
-# Author: Alexia Michelle  <alexiarstein@aol.com>
+# Author: Alexia Michelle <alexiarstein@aol.com>
+# For debian & GoldenDog systems
+# License: MIT (See LICENSE.md for more info)
+# Use: Run manually or ideally, add it as a cronjob to run weekly or monhtly.
+# ---------------------------------------------------------------------------
 
-# Variables:
-
-boot_dir="/boot" # Define the directory where cleaning is performed
-kernel_files=$(ls vmlinuz-* initrd.img-* System.map-*)
-kernels_to_keep=2 # How many kernels do we want to keep in the system
-
-# Do not edit anything below (Unless you know what you're doing)
-
+boot_dir="/boot"
 cd "$boot_dir" || exit
-sorted_files=$(echo "$kernel_files" | sort -r)
+kernel_versions=$(ls vmlinuz-* | sed 's/^vmlinuz-//')
+sorted_versions=$(echo "$kernel_versions" | sort -r)
+
+# How many versions do we want to keep? Increase this number if you want to keep more than 2
+kernels_to_keep=2
+# Do not edit anything below, unless you know what you're doing :V
+
 count=0
-for file in $sorted_files; do
+for version in $sorted_versions; do
     if [ $count -ge $kernels_to_keep ]; then
-        echo "Removing old kernel: $file"
-        sudo rm "$file"
-        sudo rm "$(basename "$file" vmlinuz-*).img"
-        sudo rm "$(basename "$file" vmlinuz-*)"
-        sudo rm "$(basename "$file" vmlinuz-*)"
+        echo "Removing old kernel version: $version"
+        sudo rm "vmlinuz-$version"
+        sudo rm "initrd.img-$version"
+        sudo rm "System.map-$version"
+        sudo rm "config-$version"
     fi
     ((count++))
 done
